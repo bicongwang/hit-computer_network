6a47b9814c1c6bfcf17abeb50d1f14ec5a0332c3
wangbicong <wangbicong@hotmail.com>
2016-10-24 23:38:57 +0800

    完善之前的功能并添加大量注释

--- proxy/main.py
+++ proxy/main.py
@@ -1,18 +1,27 @@
 # -*- coding:utf-8 -*-
+import os
 import socket
 import thread
 from proxy import Proxy
 
 PORT = 5000
-THREAD = 10
+THREAD = 5
 
 if __name__ == '__main__':
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('', PORT))
-    server.listen(THREAD)
+    # 设置网络连接为ipv4， 传输层协议为tcp
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 传输完成后立即回收该端口
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # 任意ip均可以访问
+    s.bind(('', PORT))
+    # 控制队列中可等待的最大链接
+    s.listen(THREAD)
+
+    # 创建缓存目录
+    if not os.path.exists('./cache'):
+        os.mkdir('./cache')
 
     while 1:
         # print 'Create new thread.\n'
-        thread.start_new_thread(Proxy(server).run, ())
+        thread.start_new_thread(Proxy(s).run, ())
--- proxy/proxy.py
+++ proxy/proxy.py
@@ -14,21 +14,19 @@ class Proxy(object):
     HTTP_METHOD = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']
     HTTPS_METHOD = ['CONNECT']
 
-    def __init__(self, server):
-        self.client, (self.client_ip, self.client_port) = server.accept()
+    def __init__(self, server_sock):
+        self.client_sock, (self.client_ip, self.client_port) = server_sock.accept()
 
     def parse_method(self):
-        self.request = self.client.recv(self.BUFFER_SIZE)
 
-        if not self.request:
+        self.request = self.client_sock.recv(self.BUFFER_SIZE)  # 获取HTTP请求报文
+
+        if not self.request:                              # 未获取报文直接返回
             return
 
         # print self.request
-        lines = self.request.split('\r\n')                # 解析html协议的各行
-
-        self.method = lines[0].split()[0]
-        self.url = lines[0].split()[1]
-
+        lines = self.request.split('\r\n')
+        self.method = lines[0].split()[0]          # 解析method，区分协议
 
     def run(self):
         self.parse_method()
@@ -42,12 +40,10 @@ class Proxy(object):
 
         self.parse_url_http()
 
+        # 判断是否在防火墙里
         if not self.filter_fire_wall():
 
-            del_url = self.scheme + '://' + self.host
-            request = self.request.replace(del_url, '')
-            # print request
-
+            # 确认该host和port可连接
             try:
                 addr_info = socket.getaddrinfo(self.host, self.port)[0]
             except socket.gaierror, e:
@@ -58,83 +54,93 @@ class Proxy(object):
                 print 'The address information is ' + addr_info
                 sys.exit(1)
             else:
-                self.target = socket.socket(addr_info[0], addr_info[1])
-                self.target.connect(addr_info[4])
-                self.target.send(request)
+                # 对目标服务器建立socket连接
+                self.target_sock = socket.socket(addr_info[0], addr_info[1])
+                self.target_sock.connect(addr_info[4])
+                self.target_sock.send(self.request)
                 self.return_data()
 
     def parse_url_http(self):
+
+        lines = self.request.split('\r\n')
+        self.url = lines[0].split()[1]
+
         parse_url = urlparse(self.url)
         self.host = parse_url.netloc
         self.path = parse_url.path
-        self.scheme = parse_url.scheme
 
-        if ':' in self.host:
+        if ':' in self.host:             # 处理url附带端口的情况
             self.port = self.host.split(':')[1]
             self.host = self.host.split(':')[0]
         else:
-            self.port = 80
+            self.port = 80               # http协议默认端口为80
 
-            print self.method, self.url
-            # print self.host, self.client_ip
+        print self.method, self.url
+        # print self.host, self.client_ip
 
     def https_request(self):
         pass
 
     def return_data(self):
-        inputs = [self.target, self.client]
+        inputs = [self.target_sock, self.client_sock]
 
-        with open('cache/' + self.url.replace('/', '%')[:100], 'w') as f:
-
+        fish_data = self.fish()
+        # 判断是否为钓鱼网站
+        if fish_data:
+            self.client_sock.send(fish_data)
+        else:
             while True:
-                readable, writeable, errs = select.select(inputs, [], inputs, 3)
-                if errs:
+                # 监控可读的socket对象
+                readable, writeable, errors = select.select(inputs, [], inputs, 3)
+
+                # 存在错误则直接退出
+                if errors:
                     break
+
+                # 建立过tcp连接后则直接交换客户端和目标服务器的数据
                 for socket in readable:
                     data = socket.recv(self.BUFFER_SIZE)
                     if data:
-                        if socket is self.target:
-                            self.client.send(data)
-                            f.write(data)
-                        elif socket is self.client:
-                            self.target.send(data)
+                        if socket is self.target_sock:
+                            self.client_sock.send(data)
+                        elif socket is self.client_sock:
+                            self.target_sock.send(data)
                     else:
                         break
 
-        self.client.close()
-        self.target.close()
+        # 无数据传输后关闭socket连接
+        self.client_sock.close()
+        self.target_sock.close()
 
     def filter_fire_wall(self):
+        # 防火墙，限制用户ip以及访问的网站
 
         with open('filter.json', 'r') as f:
             filter_json = json.load(f)
 
             if self.client_ip in filter_json['ip']:
-                print 'Filter client ip.'
+                print 'Filter client ip %s.' % self.client_ip
                 return True
 
             for filter_host in filter_json['host']:
                 if self.host.endswith(filter_host):
-                    print 'Filter host.'
+                    print 'Filter host %s.' % filter_host
                     return True
-
-            for fish in filter_json['fishing']:
-                """
-                钓鱼网站（有时候刷新的时候出现问题）
-                """
-                if self.host == fish:
-                    # print 'The url ' + self.url,
-
-                    last_host = self.host
-                    self.host = filter_json['fishing'][fish]
-
-                    last_url = self.url
-                    self.url = self.url.replace(fish, filter_json['fishing'][fish])
-
-                    self.request = self.request.replace(last_host, self.host)
-                    self.request = self.request.replace(last_url, self.url)
-
-                    # print 'is redirect to ' + self.url
 
         return False
 
+    def fish(self):
+
+        # 钓鱼网站
+        with open('filter.json', 'r') as f:
+
+            filter_json = json.load(f)
+
+            for fish in filter_json['fishing']:
+                if self.host == fish and self.path == '/':
+
+                    # 返回重定向相应
+                    return str('HTTP/1.1 302 Moved Temporarily\r\nLocation: http://' + \
+                    filter_json['fishing'][self.host] + '\r\n\r\n')
+
+        return False
